2d05e109b676e318db39b8936e1c0ffaf1db1327
SantiZai <santyagozaidan@gmail.com>
2024-05-08 12:15:03 -0300

    add the new services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
 import { PrismaService } from './prisma.service';
+import { CourtsService } from './courts/courts.service';
+import { CourtsController } from './courts/courts.controller';
+import { ClubsService } from './clubs/clubs.service';
+import { ClubsController } from './clubs/clubs.controller';
+import { ReservationsService } from './reservations/reservations.service';
+import { ReservationsController } from './reservations/reservations.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, PrismaService, UsersService],
+  controllers: [AppController, UsersController, CourtsController, ClubsController, ReservationsController],
+  providers: [AppService, PrismaService, UsersService, CourtsService, ClubsService, ReservationsService],
 })
 export class AppModule {}
