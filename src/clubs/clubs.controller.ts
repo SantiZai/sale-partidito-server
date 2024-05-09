e12cc05792b4db05c37b4a8070706a0e1a09d4b2
SantiZai <santyagozaidan@gmail.com>
2024-05-09 19:40:24 -0300

    get post and delete options added to users

--- src/clubs/clubs.controller.ts
+++ src/clubs/clubs.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ClubsService } from './clubs.service';
+import { Club, SPORTS } from '@prisma/client';
 
 @Controller('clubs')
-export class ClubsController {}
+export class ClubsController {
+  constructor(private clubsService: ClubsService) {}
+
+  @Get(':location/:sport')
+  async findByLocationAndSport(
+    @Param('location') location: string,
+    @Param('sport') sport: SPORTS,
+  ): Promise<Club[] | null> {
+    return await this.clubsService.getClubsByLocationAndSport(location, sport);
+  }
+
+  @Get(':location')
+  async findByLocation(
+    @Param('location') location: string,
+  ): Promise<Club[] | null> {
+    return await this.clubsService.getClubsByLocation(location);
+  }
+
+  @Post()
+  async create(@Body() club: Club): Promise<Club> {
+    return await this.clubsService.createClub(club);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Club | null> {
+    return await this.clubsService.deleteClub(id);
+  }
+}
--- src/clubs/clubs.service.ts
+++ src/clubs/clubs.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Club, SPORTS } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ClubsService {}
+export class ClubsService {
+  constructor(private prisma: PrismaService) {}
+
+  async getClubsByLocation(location: string): Promise<Club[] | null> {
+    const clubs = this.prisma.club.findMany({
+      where: {
+        location,
+      },
+      include: {
+        courts: {
+          include: {
+            reservations: true,
+          },
+        },
+      },
+    });
+    if (clubs) return clubs;
+    throw new NotFoundException(
+      'No se ha encontrado algún club en esa ubicación',
+    );
+  }
+
+  async getClubsByLocationAndSport(
+    location: string,
+    sport: SPORTS,
+  ): Promise<Club[] | null> {
+    const clubs = this.prisma.club.findMany({
+      where: {
+        location,
+        sports: {
+          has: sport,
+        },
+      },
+    });
+    if (clubs) return clubs;
+    throw new NotFoundException(
+      'No se ha encontrado algún club en esa ubicación y con ese deporte disponible',
+    );
+  }
+
+  async createClub(club: Club): Promise<Club> {
+    const userToUpdate = await this.prisma.user.findUnique({
+      where: {
+        id: club.adminId,
+      },
+    });
+    await this.prisma.user.update({
+      where: {
+        id: userToUpdate.id,
+      },
+      data: {
+        ...userToUpdate,
+        userType: 'superadmin',
+      },
+    });
+    const newClub = await this.prisma.club.create({
+      data: {
+        ...club,
+        name: club.name.trim().toLowerCase().split(' ').join('-'),
+        // deberá recibir location del tipo ciudad, provincia, país
+        location: club.location.trim().toLowerCase().split(' ').join(''),
+      },
+    });
+    return newClub;
+  }
+
+  async deleteClub(id: string): Promise<Club | null> {
+    return await this.prisma.club.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
