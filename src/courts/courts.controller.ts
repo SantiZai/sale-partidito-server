46dd163cd5b081a9d30cf9264d39c1f314453242
SantiZai <santyagozaidan@gmail.com>
2024-05-16 12:03:43 -0300

    get by id, by club id and create courts

--- src/courts/courts.controller.ts
+++ src/courts/courts.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CourtsService } from './courts.service';
+import { Court } from '@prisma/client';
 
 @Controller('courts')
-export class CourtsController {}
+export class CourtsController {
+  constructor(private courtsService: CourtsService) {}
+
+  @Get('club/:id')
+  async findByClubId(@Param('id') id: string): Promise<Court[] | null> {
+    return await this.courtsService.getCourtsByClubId(id);
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<Court | null> {
+    return await this.courtsService.getCourtById(id);
+  }
+
+  @Post()
+  async create(@Body() court: Court): Promise<Court> {
+    return await this.courtsService.createCourt(court);
+  }
+}
--- src/courts/courts.service.ts
+++ src/courts/courts.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Court } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class CourtsService {}
+export class CourtsService {
+  constructor(private prisma: PrismaService) {}
+
+  async getCourtsByClubId(clubId: string): Promise<Court[] | null> {
+    const courts = await this.prisma.court.findMany({
+      where: {
+        clubId,
+      },
+      include: {
+        reservations: true,
+      },
+    });
+    if (courts) return courts;
+    throw new NotFoundException('No se han encontrado canchas en este club');
+  }
+
+  async getCourtById(id: string): Promise<Court | null> {
+    const court = await this.prisma.court.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        reservations: true,
+      },
+    });
+    if (court) return court;
+    throw new NotFoundException(
+      'No se ha encontrado alguna cancha con este id',
+    );
+  }
+
+  async createCourt(court: Court): Promise<Court> {
+    const newCourt = await this.prisma.court.create({
+      data: court,
+    });
+    return newCourt;
+  }
+}
